78f6eefdf80fb8dc7f8a8a7da083f2e3800b89c3
DarkInk <rstonehouse66@gmail.com>
2021-02-17 06:54:43 +0000

    warning commands

---
+++ handlers/commands/warns.js
@@ -0,0 +1,20 @@
+const db = require("quick.db")
+
+module.exports = {
+  name: "warns",
+  description: "Get the warns of yours or mentioned person",
+    execute: async (message, args, client) => {
+    const user = message.mentions.members.first() || message.author
+    
+  
+    let warnings = db.get(`warnings_${message.guild.id}_${user.id}`)
+    
+    
+    if(warnings === null) warnings = 0;
+    
+    
+    message.channel.send(`${user} have **${warnings}** warning(s)`)
+  
+  
+  }
+}
