a2faf3b30204e243a0d117bf9fc84ade6cc31cd3
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-11 18:34:14 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const client = new Discord.Client();
+
+function getDirectories() {
+  return fs.readdirSync("./handlers/commands/").filter(function subFolders(file) {
+    return fs.statSync("./handlers/commands/" + file).isDirectory();
+  });
+}
+const commandFiles = fs
+  .readdirSync("./handlers/commands/")
+  .filter((file) => file.endsWith(".js"));
+for (const folder of getDirectories()) {
+  const folderFiles = fs
+    .readdirSync("./handlers/commands/" + folder)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of folderFiles) {
+    commandFiles.push([folder, file]);
+  }
+}
+for (const file of commandFiles) {
+  let command;
+  if (Array.isArray(file)) {
+    command = require(`./handlers/commands/${file[0]}/${file[1]}`);
+  } else {
+    command = require(`./handlers/commands/${file}`);
+  }
+  console.log(colors.green(`✅  Success! Command: ${command.name} Is Working!`));
+}
+
+client.login(process.env.TOKEN);
+console.log('The Bot has Logged In and Works')
