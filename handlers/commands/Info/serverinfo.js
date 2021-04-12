660196da593eacff9fe4e9d59b06c46271c35e7b
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-12 09:16:45 -0500

    Update serverinfo.js

--- handlers/commands/Info/serverinfo.js
+++ handlers/commands/Info/serverinfo.js
@@ -1,34 +1,36 @@
-const { MessageEmbed } = require('discord.js');
+const {
+    MessageEmbed
+} = require('discord.js');
 const moment = require('moment');
 
 const filterLevels = {
-	DISABLED: 'Off',
-	MEMBERS_WITHOUT_ROLES: 'No Role',
-	ALL_MEMBERS: 'Everyone'
+    DISABLED: 'Off',
+    MEMBERS_WITHOUT_ROLES: 'No Role',
+    ALL_MEMBERS: 'Everyone'
 };
 
 const verificationLevels = {
-	NONE: 'None',
-	LOW: 'Low',
-	MEDIUM: 'Medium',
-	HIGH: 'High',
-	VERY_HIGH: 'Very High'
+    NONE: 'None',
+    LOW: 'Low',
+    MEDIUM: 'Medium',
+    HIGH: 'High',
+    VERY_HIGH: 'Very High'
 };
 
 const regions = {
-	brazil: 'Brazil',
-	europe: 'Europe',
-	hongkong: 'Hong Kong',
-	india: 'India',
-	japan: 'Japan',
-	russia: 'Russia',
-	singapore: 'Singapore',
-	southafrica: 'South Africa',
-	sydeny: 'Sydeny',
-	'us-central': 'US Central',
-	'us-east': 'US East',
-	'us-west': 'US West',
-	'us-south': 'US South'
+    brazil: 'Brazil',
+    europe: 'Europe',
+    hongkong: 'Hong Kong',
+    india: 'India',
+    japan: 'Japan',
+    russia: 'Russia',
+    singapore: 'Singapore',
+    southafrica: 'South Africa',
+    sydeny: 'Sydeny',
+    'us-central': 'US Central',
+    'us-east': 'US East',
+    'us-west': 'US West',
+    'us-south': 'US South'
 };
 
 module.exports = {
@@ -38,66 +40,67 @@ module.exports = {
     usage: "",
     category: "info",
     execute: async (message, args, Fall) => {
-      try {
-
-    // const roles = message.guild.roles.cache.sort((a, b) => b.position - a.position).map(role => role.toString());
-		const members = message.guild.members.cache;
-		const channels = message.guild.channels.cache;
-		const emojis = message.guild.emojis.cache;
-    let rolemap = message.guild.roles.cache
-
-
-    var roles = ''
-    var rc = 0
-
-    message.guild.roles.cache.array().forEach((i) => {roles += `<@&${i.id}>, `; rc++})
-
-		const embed = new MessageEmbed()
-			.setDescription(`**Guild information for __${message.guild.name}__**`)
-			.setColor('BLUE')
-			.setThumbnail(message.guild.iconURL({ dynamic: true }))
-			.addField('General', [
-				`**» Name:** ${message.guild.name}`,
-				`**» ID:** ${message.guild.id}`,
-				`**» Owner:** ${message.guild.owner.user.tag} (${message.guild.ownerID})`,
-				`**» Region:** ${regions[message.guild.region]}`,
-				`**» Boost Tier:** ${message.guild.premiumTier ? `Tier ${message.guild.premiumTier}` : 'None'}`,
-				`**» Explicit Filter:** ${filterLevels[message.guild.explicitContentFilter]}`,
-				`**» Verification Level:** ${verificationLevels[message.guild.verificationLevel]}`,
-				`**» Time Created:** ${moment(message.guild.createdTimestamp).format('LT')} ${moment(message.guild.createdTimestamp).format('LL')} ${moment(message.guild.createdTimestamp).fromNow()}`,
-				'\u200b'
-			])
-			.addField('Statistics', [
-				`**» Role Count:** ${rc}`,
-				`**» Emoji Count:** ${emojis.size}`,
-				`**» Regular Emoji Count:** ${emojis.filter(emoji => !emoji.animated).size}`,
-				`**» Animated Emoji Count:** ${emojis.filter(emoji => emoji.animated).size}`,
-				`**» Member Count:** ${message.guild.memberCount}`,
-				`**» Humans:** ${members.filter(member => !member.user.bot).size}`,
-				`**» Bots:** ${members.filter(member => member.user.bot).size}`,
-				`**» Text Channels:** ${channels.filter(channel => channel.type === 'text').size}`,
-				`**» Voice Channels:** ${channels.filter(channel => channel.type === 'voice').size}`,
-				`**» Boost Count:** ${message.guild.premiumSubscriptionCount || '0'}`,
-				'\u200b'
-			])
-      	.addField('Presence', [
-				`**» Online:** ${members.filter(member => member.presence.status === 'online').size}`,
-				`**» Idle:** ${members.filter(member => member.presence.status === 'idle').size}`,
-				`**» Do Not Disturb:** ${members.filter(member => member.presence.status === 'dnd').size}`,
-				`**» Offline:** ${members.filter(member => member.presence.status === 'offline').size}`,
-				'\u200b'
-			])
-      // .addField(`Roles [${roles.length - 1}]`, roles.length < 10 ? roles.join(', ') : 'None') for fuck sakes leave it 
-      // .addField(`Roles [${rc}]`, roles) danik let me add one thing what do you want to add? error message for when it gets a error
-			.setTimestamp();
-
-
-    if (rc > 30 || roles.length > 1023) embed.addField(`Roles [${rc}]`, `_To many to show If you want to see them all do p!roles_`)
-    else embed.addField(`Roles [${rc}]`, roles.slice(0, -1))
-		message.channel.send(embed);
+        try {
+            
+            const members = message.guild.members.cache;
+            const channels = message.guild.channels.cache;
+            const emojis = message.guild.emojis.cache;
+            let rolemap = message.guild.roles.cache
+            
+            
+            var roles = ''
+            var rc = 0
+            
+            message.guild.roles.cache.array().forEach((i) => {
+                roles += `<@&${i.id}>, `;
+                rc++
+            })
+            
+            const embed = new MessageEmbed()
+                .setDescription(`**Guild information for __${message.guild.name}__**`)
+                .setColor('BLUE')
+                .setThumbnail(message.guild.iconURL({
+                    dynamic: true
+                }))
+                .addField('General', [
+                    `**» Name:** ${message.guild.name}`,
+                    `**» ID:** ${message.guild.id}`,
+                    `**» Owner:** ${message.guild.owner.user.tag} (${message.guild.ownerID})`,
+                    `**» Region:** ${regions[message.guild.region]}`,
+                    `**» Boost Tier:** ${message.guild.premiumTier ? `Tier ${message.guild.premiumTier}` : 'None'}`,
+                    `**» Explicit Filter:** ${filterLevels[message.guild.explicitContentFilter]}`,
+                    `**» Verification Level:** ${verificationLevels[message.guild.verificationLevel]}`,
+                    `**» Time Created:** ${moment(message.guild.createdTimestamp).format('LT')} ${moment(message.guild.createdTimestamp).format('LL')} ${moment(message.guild.createdTimestamp).fromNow()}`,
+                    '\u200b'
+                ])
+                .addField('Statistics', [
+                    `**» Role Count:** ${rc}`,
+                    `**» Emoji Count:** ${emojis.size}`,
+                    `**» Regular Emoji Count:** ${emojis.filter(emoji => !emoji.animated).size}`,
+                    `**» Animated Emoji Count:** ${emojis.filter(emoji => emoji.animated).size}`,
+                    `**» Member Count:** ${message.guild.memberCount}`,
+                    `**» Humans:** ${members.filter(member => !member.user.bot).size}`,
+                    `**» Bots:** ${members.filter(member => member.user.bot).size}`,
+                    `**» Text Channels:** ${channels.filter(channel => channel.type === 'text').size}`,
+                    `**» Voice Channels:** ${channels.filter(channel => channel.type === 'voice').size}`,
+                    `**» Boost Count:** ${message.guild.premiumSubscriptionCount || '0'}`,
+                    '\u200b'
+                ])
+                .addField('Presence', [
+                    `**» Online:** ${members.filter(member => member.presence.status === 'online').size}`,
+                    `**» Idle:** ${members.filter(member => member.presence.status === 'idle').size}`,
+                    `**» Do Not Disturb:** ${members.filter(member => member.presence.status === 'dnd').size}`,
+                    `**» Offline:** ${members.filter(member => member.presence.status === 'offline').size}`,
+                    '\u200b'
+                ])
+                .setTimestamp();
+            
+            
+            if (rc > 30 || roles.length > 1023) embed.addField(`Roles [${rc}]`, `_To many to show If you want to see them all do p!roles_`)
+            else embed.addField(`Roles [${rc}]`, roles.slice(0, -1))
+            message.channel.send(embed);
+        } catch (error) {
+            message.reply(`There was an using this command, \n\n**${error}**`);
+        }
     }
-    catch (error) {
-      message.reply(`There was an using this command, \n\n**${error}**`);
-    }
-}
 }
