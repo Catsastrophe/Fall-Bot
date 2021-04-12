8b566fd17f6ced1127ccd98fc2878ae7a1ad3c3b
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-12 10:16:25 -0500

    Update cat-fact.js

--- handlers/commands/Random/cat-fact.js
+++ handlers/commands/Random/cat-fact.js
@@ -1,20 +1,23 @@
 const catFacts = require('../../../json/cat-fact.json');
-const { MessageEmbed } = require('discord.js');
+const {
+    MessageEmbed
+} = require('discord.js');
 
 module.exports = {
     name: 'cat-fact',
-    guildOnly: true,
     description: 'Get a random cat fact',
     usage: "cat-fact",
     category: "Fun",
-
-  execute: async (message, args) => {
-
-    const embed = new MessageEmbed()
-      .setColor("RANDOM")
-      .setTitle('🐱 | **Cat Fact**')
-
-      .setDescription(catFacts[Math.round(Math.random() * (catFacts.length - 1))])
-    message.channel.send({ embed });
-  }
+    
+    execute: async (message, args) => {
+        
+        const embed = new MessageEmbed()
+            .setColor("RANDOM")
+            .setTitle('🐱 | **Cat Fact**')
+            
+            .setDescription(catFacts[Math.round(Math.random() * (catFacts.length - 1))])
+        message.channel.send({
+            embed
+        });
+    }
 };
