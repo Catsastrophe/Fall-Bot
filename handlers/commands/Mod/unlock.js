d56a7056ea7825664f1eed9768c8b6d6fd7ad7e0
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-12 09:31:27 -0500

    Update unlock.js

--- handlers/commands/Mod/unlock.js
+++ handlers/commands/Mod/unlock.js
@@ -1,27 +1,26 @@
 const Discord = module.require("discord.js");
 
 module.exports = {
-    name: 'Unlock',
-    guildOnly: false,
+    name: 'unlock',
     description: 'Unlock The Channel',
     usage: "Unlock [channel]",
     category: "Moderation",
-        execute: async (message, args) => {
+    execute: async (message, args) => {
         if (!message.member.hasPermission('MANAGE_MESSAGES'))
-            return message.channel.send('You do not have permission to use this command.').then(m => m.delete({timeout: 5000}));
-
-               message.channel.overwritePermissions([
-     {
-        id: message.guild.id,
-        allow : ['SEND_MESSAGES'],
-     },
-    ],);
-
-   const embed = new Discord.MessageEmbed()
-   .setTitle("Channel Updates")
-   .setDescription(` ${message.channel}s Padlock has be removed`)
-   .setColor("RANDOM");
-   await message.channel.send(embed);
-   message.delete();
-}
+            return message.channel.send('You do not have permission to use this command.').then(m => m.delete({
+                timeout: 5000
+            }));
+        
+        message.channel.overwritePermissions([{
+            id: message.guild.id,
+            allow: ['SEND_MESSAGES'],
+        }, ], );
+        
+        const embed = new Discord.MessageEmbed()
+            .setTitle("Channel Updates")
+            .setDescription(` ${message.channel}s Padlock has be removed`)
+            .setColor("RANDOM");
+        await message.channel.send(embed);
+        message.delete();
+    }
 }
