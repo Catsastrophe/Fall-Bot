521c3ba39cf71b6d3e8d7c37ba299511dd774f08
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-12 10:15:18 -0500

    Update advice.js

--- handlers/commands/Random/advice.js
+++ handlers/commands/Random/advice.js
@@ -1,19 +1,21 @@
-const { Random } = require("something-random-on-discord")
+const {
+    Random
+} = require("something-random-on-discord")
 const random = new Random();
 
 module.exports = {
-  name: "advice",
-  description: "Get some advice",
-  usage: "advice",
-  category: "Fun",
+    name: "advice",
+    description: "Get some advice",
+    usage: "advice",
+    category: "Fun",
     execute: async (message, args) => {
-
-    r = Math.random()
-    
-    let data = await random.getAdvice()
-
-    // console.log(data)
-    message.channel.send(data)
-    
-  }
+        
+        r = Math.random()
+        
+        let data = await random.getAdvice()
+        
+        // console.log(data)
+        message.channel.send(data)
+        
+    }
 }
