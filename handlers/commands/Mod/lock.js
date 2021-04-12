0a655fa3f7cf24915005777239374cf32d02ded0
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-12 09:27:39 -0500

    Formatting

--- handlers/commands/Mod/lock.js
+++ handlers/commands/Mod/lock.js
@@ -2,26 +2,25 @@ const Discord = module.require("discord.js");
 
 module.exports = {
     name: 'lock',
-    guildOnly: false,
-    description: 'Lock the channel: Bugged Atm',
+    description: 'Lock the channel',
     usage: "lock [channel]",
     category: "Moderation",
-        execute: async (message, args) => {
+    execute: async (message, args) => {
         if (!message.member.hasPermission('MANAGE_MESSAGES'))
-            return message.channel.send('You do not have permission to use this command.').then(m => m.delete({timeout: 5000}));
-
-               message.channel.overwritePermissions([
-     {
-        id: message.guild.id,
-        deny : ['SEND_MESSAGES'],
-     },
-    ],);
-
-   const embed = new Discord.MessageEmbed()
-   .setTitle("Channel Updates")
-   .setDescription(`${message.channel} Got a Padlock added to it `)
-   .setColor("RANDOM");
-   await message.channel.send(embed);
-   message.delete();
-}
+            return message.channel.send('You do not have permission to use this command.').then(m => m.delete({
+                timeout: 5000
+            }));
+        
+        message.channel.overwritePermissions([{
+            id: message.guild.id,
+            deny: ['SEND_MESSAGES'],
+        }, ], );
+        
+        const embed = new Discord.MessageEmbed()
+            .setTitle("Channel Updates")
+            .setDescription(`${message.channel} Got a Padlock added to it `)
+            .setColor("RANDOM");
+        await message.channel.send(embed);
+        message.delete();
+    }
 }
