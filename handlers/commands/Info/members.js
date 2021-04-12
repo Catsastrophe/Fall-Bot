b4b543b2725418b2caa92ea7a09efe80d9ba0833
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-12 09:11:33 -0500

    Update members.js

--- handlers/commands/Info/members.js
+++ handlers/commands/Info/members.js
@@ -1,21 +1,21 @@
 const discord = require("discord.js");
 
 module.exports = {
-  name: "membercount",
-  description: "How Many members let me think....",
-  usage: "membercount",
-  category: "info",
-  execute: async (message, args, client) => {
-    
-    let embed = new discord.MessageEmbed()
-    .setDescription(
-    `
+    name: "membercount",
+    description: "How Many members let me think....",
+    usage: "membercount",
+    category: "info",
+    execute: async (message, args, client) => {
+        
+        let embed = new discord.MessageEmbed()
+            .setDescription(
+                `
 Total Members - ${message.guild.memberCount}
 Humans - ${message.guild.members.cache.filter(m => !m.user.bot).size}
 Bots - ${message.guild.members.cache.filter(m => m.user.bot).size}`)
-    .setColor("RANDOM")
-    .setTimestamp(message.timestamp = Date.now())
-    
-    message.channel.send(embed)
-  }
+            .setColor("RANDOM")
+            .setTimestamp(message.timestamp = Date.now())
+        
+        message.channel.send(embed)
+    }
 }
