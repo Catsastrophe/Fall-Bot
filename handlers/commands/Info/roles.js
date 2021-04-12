55d00e643fa139e48aa0e6bb1716e07f34fb3025
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-12 09:14:44 -0500

    Update roles.js

--- handlers/commands/Info/roles.js
+++ handlers/commands/Info/roles.js
@@ -1,65 +1,37 @@
-const { MessageEmbed } = require('discord.js');
-const moment = require('moment');
-
-const filterLevels = {
-	DISABLED: 'Off',
-	MEMBERS_WITHOUT_ROLES: 'No Role',
-	ALL_MEMBERS: 'Everyone'
-};
-
-const verificationLevels = {
-	NONE: 'None',
-	LOW: 'Low',
-	MEDIUM: 'Medium',
-	HIGH: 'High',
-	VERY_HIGH: 'Very High'
-};
-
-const regions = {
-	brazil: 'Brazil',
-	europe: 'Europe',
-	hongkong: 'Hong Kong',
-	india: 'India',
-	japan: 'Japan',
-	russia: 'Russia',
-	singapore: 'Singapore',
-	southafrica: 'South Africa',
-	sydeny: 'Sydeny',
-	'us-central': 'US Central',
-	'us-east': 'US East',
-	'us-west': 'US West',
-	'us-south': 'US South'
-};
+const {
+    MessageEmbed
+} = require('discord.js');
 
 module.exports = {
     name: "roles",
-    description: "Get the server info",
-    aliases: ["sein", "infoserver", "sp"],
+    description: "Get All the roles in a server",
+    aliases: [],
     usage: "",
     category: "info",
     execute: async (message, args, Fall) => {
-      try {
-
-    // const roles = message.guild.roles.cache.sort((a, b) => b.position - a.position).map(role => role.toString());
-		const members = message.guild.members.cache;
-		const channels = message.guild.channels.cache;
-		const emojis = message.guild.emojis.cache;
-    let rolemap = message.guild.roles.cache
-
-
-    var roles = ''
-    var rc = 0
-
-    message.guild.roles.cache.array().forEach((i) => {roles += `<@&${i.id}>, `; rc++})
-
-		const embed = new MessageEmbed()
-			.setTimestamp();
-
-
-    if (rc > 100 || roles.length > 1023) embed.addField(`Roles [${rc}]`, `You still have wayyy too many roles It goes over the 1024 word length Sorry I just can't show them`)
-    else embed.addField(`Roles [${rc}]`, roles.slice(0, -1))
-		message.channel.send(embed);
+        try {
+            
+            const members = message.guild.members.cache;
+            const channels = message.guild.channels.cache;
+            const emojis = message.guild.emojis.cache;
+            let rolemap = message.guild.roles.cache
+            
+            
+            var rc = 0
+            const roles = message.guild.roles.cache
+                .filter(r => r.id !== message.guild.id)
+                .map(r => r).join(", ") || 'none';
+            
+            for (let i = 0; i < roles.length; i += 2000) {
+                const toSend = roles.substring(i, Math.min(roles.length, i + 2000));
+                let embed = new MessageEmbed()
+                    .setColor("RANDOM")
+                    .setTitle(`Roles`)
+                    .setDescription(`${toSend}, @everyone`)
+                message.channel.send(embed)
+            }
+        } catch (error) {
+            message.reply(`There was an using this command, \n\n**${error.stack}**`);
+        }
     }
-    catch (error) {
-      message.reply(`There was an using this command, \n\n**${error}**`);
-    }}}
+}
