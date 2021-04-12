5ea35318e1411a59a6fd9c6e14350af4330fc58e
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-12 09:33:22 -0500

    Update eval.js

--- handlers/commands/Owner/eval.js
+++ handlers/commands/Owner/eval.js
@@ -1,30 +1,30 @@
-const { MessageEmbed } = require('discord.js');
-const { inspect } = require('util');
+const {
+    MessageEmbed
+} = require('discord.js');
+const {
+    inspect
+} = require('util');
 const config = require('../../../conf/config.json');
 const fs = require('fs')
 
 module.exports = {
     name: 'eval',
-    guildOnly: false,
-    ownerOnly: true,
     description: 'Find Out how the dumbest person Is',
     category: "Fun",
-  execute: async (message, agrs, client) => {
-  const args = message.content.split(' ');
-  const command = args.shift().toLowerCase();
-if (!config.owners.includes(message.author.id)) {
+    execute: async (message, agrs, client) => {
+        const args = message.content.split(' ');
+        const command = args.shift().toLowerCase();
+        if (!config.owners.includes(message.author.id)) {
             return message.channel.send(`You can\'t use this`)
         }
-    let evaled;
-    try {
-      evaled = await eval(args.join(' '));
-      message.channel.send(inspect(evaled));
-      console.log(inspect(evaled));
-  }
-    
-catch (error) {
-      console.log(error);
-      message.reply(`There was an error during evaluation, \n\n**${error}**`);
+        let evaled;
+        try {
+            evaled = await eval(args.join(' '));
+            message.channel.send(inspect(evaled));
+            console.log(inspect(evaled));
+        } catch (error) {
+            console.log(error);
+            message.reply(`There was an error during evaluation, \n\n**${error}**`);
+        }
     }
-  }
 };
