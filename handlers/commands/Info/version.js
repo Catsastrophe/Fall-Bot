1748e855e071a3acac7002ad9b5a4878e37a79f2
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-12 09:22:24 -0500

    Update version.js

--- handlers/commands/Info/version.js
+++ handlers/commands/Info/version.js
@@ -1,4 +1,6 @@
-const { MessageEmbed } = require("discord.js");
+const {
+    MessageEmbed
+} = require("discord.js");
 const configs = require('../../../conf/config.json');
 const Discord = require('discord.js');
 const color = require('../../../conf/colors.json');
@@ -9,17 +11,17 @@ module.exports = {
     usage: "",
     category: "Utility",
     async execute(message, args) {
-    
-    const embed = new MessageEmbed()
-
-    .setTitle("**Version**")
-
-    .setDescription(`The version The Bot is on is **${configs.version}**, Thank you for using Fall Music **Premium**, *Enjoy The Music*`)
-    
-    .setColor(color.Pumpkin)
-
-    .setTimestamp()
-
-    return message.channel.send(embed);
+        
+        const embed = new MessageEmbed()
+            
+            .setTitle("**Version**")
+            
+            .setDescription(`The version The Bot is on is **${configs.version}**, Thank you for using Fall Music **Premium**, *Enjoy The Music*`)
+            
+            .setColor(color.Pumpkin)
+            
+            .setTimestamp()
+        
+        return message.channel.send(embed);
     }
 }
