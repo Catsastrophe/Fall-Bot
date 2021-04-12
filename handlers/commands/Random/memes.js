8fc52f3d403feb77bf40110431dff555eb260c39
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-12 10:18:21 -0500

    Update memes.js

--- handlers/commands/Random/memes.js
+++ handlers/commands/Random/memes.js
@@ -1,7 +1,9 @@
 const Discord = require("discord.js");
-const { MessageEmbed } = require("discord.js");
+const {
+    MessageEmbed
+} = require("discord.js");
 const Color = `RANDOM`;
-const Fetch = require("node-fetch"); 
+const Fetch = require("node-fetch");
 
 module.exports = {
     name: "meme",
@@ -9,9 +11,9 @@ module.exports = {
     usage: "get a random meme",
     category: "Fun",
     execute: async (message, args, client) => {
-
+        
         //Start
-
+        
         const Reds = [
             "memes",
             "me_irl",
@@ -19,17 +21,17 @@ module.exports = {
             "comedyheaven",
             "crappydesign"
         ];
-
+        
         const Rads = Reds[Math.floor(Math.random() * Reds.length)];
-
+        
         const res = await Fetch(`https://www.reddit.com/r/${Rads}/random/.json`);
-
+        
         const json = await res.json();
-
+        
         if (!json[0]) return message.channel.send(`Your Life Lmfao`);
-
+        
         const data = json[0].data.children[0].data;
-
+        
         const Embed = new MessageEmbed()
             .setColor(Color)
             .setURL(`https://reddit.com${data.permalink}`)
@@ -38,10 +40,10 @@ module.exports = {
             .setImage(data.url)
             .setFooter(`${data.ups || 0} 👍 | ${data.downs || 0} 👎 | ${data.num_comments || 0} 💬`)
             .setTimestamp();
-
+        
         return message.channel.send(Embed);
-
+        
         //End
-
+        
     }
 };
