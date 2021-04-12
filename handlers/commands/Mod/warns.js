cd0697690f0d0ab0ac753fc438baacd60ddf85b2
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-12 09:32:37 -0500

    Update warns.js

--- handlers/commands/Mod/warns.js
+++ handlers/commands/Mod/warns.js
@@ -1,22 +1,22 @@
 const db = require("quick.db")
 
 module.exports = {
-  name: "warns",
-  description: "Get the warns of yours or mentioned person",
-  usage: "warns (<@mention>)",
-  category: "Moderation",
+    name: "warns",
+    description: "Get the warns of yours or mentioned person",
+    usage: "warns (<@mention>)",
+    category: "Moderation",
     execute: async (message, args, client) => {
-    const user = message.mentions.members.first() || message.author
-    
-  
-    let warnings = db.get(`warnings_${message.guild.id}_${user.id}`)
-    
-    
-    if(warnings === null) warnings = 0;
-    
-    
-    message.channel.send(`${user} have **${warnings}** warning(s)`)
-  
-  
-  }
+        const user = message.mentions.members.first() || message.author
+        
+        
+        let warnings = db.get(`warnings_${message.guild.id}_${user.id}`)
+        
+        
+        if (warnings === null) warnings = 0;
+        
+        
+        message.channel.send(`${user} have **${warnings}** warning(s)`)
+        
+        
+    }
 }
