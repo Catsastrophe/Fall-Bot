624a398e12b1443e48bc95c52d682e8e5cbf806d
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-12 09:18:29 -0500

    Update stats.js

--- handlers/commands/Info/stats.js
+++ handlers/commands/Info/stats.js
@@ -1,4 +1,6 @@
-const { MessageEmbed } = require('discord.js');
+const {
+    MessageEmbed
+} = require('discord.js');
 const configs = require('../../../conf/config.json');
 const moment = require("moment");
 require("moment-duration-format");
@@ -10,20 +12,22 @@ module.exports = {
     usage: "stats",
     category: "info",
     execute: async (message, args, client) => {
-
-      const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-
-    const embed = new MessageEmbed()
-      .setTitle("__**Fall Music Stats**__")
-      .setColor('RANDOM')
-      .addField(':clock4: | **Uptime**', `${duration}`, true)
-      .addField(':bar_chart: | **Server Count**', `${client.guilds.cache.size.toLocaleString()}` , true)
-      .addField(':chart_with_upwards_trend: | **User Count**', `${client.users.cache.size.toLocaleString()}`, true)
-      .addField(':card_box: | **Channel Count**', `${client.channels.cache.size.toLocaleString()}`, true)
-      .addField(':chart_with_downwards_trend: | **Memory Usage**', `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
-      .addField(':page_facing_up: | **Version**', `v${configs.version}`, true)
-      .setFooter('Copyright ©2020-2021 DarkerInk#6115')
-    await message.channel.send({ embed });
-    
-  }
+        
+        const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+        
+        const embed = new MessageEmbed()
+            .setTitle("__**Fall Music Stats**__")
+            .setColor('RANDOM')
+            .addField(':clock4: | **Uptime**', `${duration}`, true)
+            .addField(':bar_chart: | **Server Count**', `${client.guilds.cache.size.toLocaleString()}`, true)
+            .addField(':chart_with_upwards_trend: | **User Count**', `${client.users.cache.size.toLocaleString()}`, true)
+            .addField(':card_box: | **Channel Count**', `${client.channels.cache.size.toLocaleString()}`, true)
+            .addField(':chart_with_downwards_trend: | **Memory Usage**', `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
+            .addField(':page_facing_up: | **Version**', `v${configs.version}`, true)
+            .setFooter('Copyright ©2020-2021 DarkerInk#6115')
+        await message.channel.send({
+            embed
+        });
+        
+    }
 }
