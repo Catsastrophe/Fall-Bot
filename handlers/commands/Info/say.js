f7d1aed57bca1ceef62574be8e73d6db35718cd5
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-12 09:15:31 -0500

    Update say.js

--- handlers/commands/Info/say.js
+++ handlers/commands/Info/say.js
@@ -5,25 +5,28 @@ const {
 const colors = require('../../../conf/colors.json');
 module.exports = {
     name: 'say',
-    guildOnly: true,
     description: 'Make the bot say that nasty thing',
     usage: "say <message>",
     category: "Fun",
     execute: async (message, args) => {
-    const embed = new Discord.MessageEmbed()
+        const embed = new Discord.MessageEmbed()
         message.delete()
-
+        
         if (!message.member.hasPermission('MANAGE_MESSAGES'))
-            return message.channel.send('You do not have permission to use this command.').then(m => m.delete({timeout: 5000}));
+            return message.channel.send('You do not have permission to use this command.').then(m => m.delete({
+                timeout: 5000
+            }));
         
         if (args.length < 1)
-            return message.channel.send('You must specify something for the bot to repeat!').then(m => m.delete({timeout: 5000}));
-
+            return message.channel.send('You must specify something for the bot to repeat!').then(m => m.delete({
+                timeout: 5000
+            }));
+        
         if (args[0].toLowerCase() === 'embed') {
             const embed = new MessageEmbed()
                 .setColor(process.env.COLOR)
                 .setDescription(args.slice(1).join(' '))
-
+            
             message.channel.send(embed);
         } else {
             message.channel.send(args.join(' '));
