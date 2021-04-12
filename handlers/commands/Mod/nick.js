ca60ea0b837a7cdd2995a7fb5e278fa883cb1c57
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-12 09:28:26 -0500

    Update nick.js

--- handlers/commands/Mod/nick.js
+++ handlers/commands/Mod/nick.js
@@ -1,18 +1,19 @@
-const { MessageEmbed } = require("discord.js");
+const {
+    MessageEmbed
+} = require("discord.js");
 module.exports = {
-  name: "nick",
-  category: "Moderation",
-  description: "Nick someone that Nickname breaks the rules",
-  execute: async (message, args) => {
-        if(message.channel.type == 'DM') return message.reply('You can use this command only in servers');
+    name: "nick",
+    category: "Moderation",
+    description: "Change the Nickname of a given user",
+    execute: async (message, args) => {
         if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('I don\'t have permission to change nicknames!');
-        if(!message.member.hasPermission('MANAGE_NICKNAMES')) return message.reply("You haven't the permission to execute this command!");
+        if (!message.member.hasPermission('MANAGE_NICKNAMES')) return message.reply("You haven't the permission to execute this command!");
         let mentionMember = message.mentions.members.first();
-
+        
         let newNickname = args.slice(1).join(' ');
-        if(!mentionMember) return message.reply("Mention the user you want to change the nickname");
-        if(!newNickname) return message.reply("Input the new nickname for the user you mentioned");
-        if(!mentionMember.kickable) return message.reply("Can't change nickname of this user, does he have a higher role? Is the server creator? Have I got the permission to change his nickname?");
+        if (!mentionMember) return message.reply("Mention the user you want to change the nickname");
+        if (!newNickname) return message.reply("Input the new nickname for the user you mentioned");
+        if (!mentionMember.kickable) return message.reply("Can't change nickname of this user, does he have a higher role? Is the server creator? Have I got the permission to change his nickname?");
         try {
             mentionMember.setNickname(newNickname);
         } catch (error) {
