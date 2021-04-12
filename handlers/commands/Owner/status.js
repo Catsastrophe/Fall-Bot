4fd3fd9ad1a24fc5121323240704dd20876d4b31
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-12 10:14:28 -0500

    Update status.js

--- handlers/commands/Owner/status.js
+++ handlers/commands/Owner/status.js
@@ -3,25 +3,23 @@ const config = require('../../../conf/config.json');
 
 module.exports = {
     name: 'status',
-    guildOnly: false,
-    ownerOnly: true,
-    description: 'Reload the commands',
-    usage: "reload <command>",
+    description: 'Change The bots status',
+    usage: "",
     category: "Owner",
     execute: async (message, args, client) => {
-      if (!config.owners.includes(message.author.id)) {
+        if (!config.owners.includes(message.author.id)) {
             return message.channel.send(`lmao are you the Owner? No So why are you trying to use this command...? <:thonking:814600683458265090>`)
         }
-const status = args[0];
-    if (!status) return message.reply("A status type must be provided.");
-
-    const statusType = args[0].toLowerCase();
-
-    if (statusType === "online" || statusType === "idle" || statusType === "dnd" || statusType === "invisible") {
-      client.user.setStatus(status);
-      message.channel.send(`☑️ | Status successfully changed to **${statusType}**.\nPlease note that initially changing status may take up to a minute or two.`);
-    } else {
-      return message.channel.send(`"${statusType}" is not a valid status type.`);
+        const status = args[0];
+        if (!status) return message.reply("A status type must be provided.");
+        
+        const statusType = args[0].toLowerCase();
+        
+        if (statusType === "online" || statusType === "idle" || statusType === "dnd" || statusType === "invisible") {
+            client.user.setStatus(status);
+            message.channel.send(`☑️ | Status successfully changed to **${statusType}**.\nPlease note that initially changing status may take up to a minute or two.`);
+        } else {
+            return message.channel.send(`"${statusType}" is not a valid status type.`);
+        }
     }
-  }
 }
