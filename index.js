1029fb4457341a3198d77a5bb38509421d1de3df
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-12 10:03:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,41 +15,41 @@ client.commands = new Discord.Collection();
 event_handler.performEvents(client);
 
 function getDirectories() {
-  return fs.readdirSync("./handlers/commands/").filter(function subFolders(file) {
-    return fs.statSync("./handlers/commands/" + file).isDirectory();
-  });
+    return fs.readdirSync("./handlers/commands/").filter(function subFolders(file) {
+        return fs.statSync("./handlers/commands/" + file).isDirectory();
+    });
 }
 const commandFiles = fs
-  .readdirSync("./handlers/commands/")
-  .filter((file) => file.endsWith(".js"));
-for (const folder of getDirectories()) {
-  const folderFiles = fs
-    .readdirSync("./handlers/commands/" + folder)
+    .readdirSync("./handlers/commands/")
     .filter((file) => file.endsWith(".js"));
-  for (const file of folderFiles) {
-    commandFiles.push([folder, file]);
-  }
+for (const folder of getDirectories()) {
+    const folderFiles = fs
+        .readdirSync("./handlers/commands/" + folder)
+        .filter((file) => file.endsWith(".js"));
+    for (const file of folderFiles) {
+        commandFiles.push([folder, file]);
+    }
 }
 for (const file of commandFiles) {
-  let command;
-  if (Array.isArray(file)) {
-    command = require(`./handlers/commands/${file[0]}/${file[1]}`);
-  } else {
-    command = require(`./handlers/commands/${file}`);
-  }
-
-  client.commands.set(command.name, command);
-  console.log(colors.green(`✅  Success! Loaded Command ${command.name} `));
+    let command;
+    if (Array.isArray(file)) {
+        command = require(`./handlers/commands/${file[0]}/${file[1]}`);
+    } else {
+        command = require(`./handlers/commands/${file}`);
+    }
+    
+    client.commands.set(command.name, command);
+    console.log(colors.green(`✅  Success! Loaded Command ${command.name} `));
 }
 
 client.on('message', message => {
-   if (message.content.startsWith(configs.prefix)) console.log(message.author.tag + ' @' + message.guild.name + ' :: ' + message.content)
-   });
+    if (message.content.startsWith(configs.prefix)) console.log(message.author.tag + ' @' + message.guild.name + ' :: ' + message.content)
+});
 
 client.on('messageDelete', message => {
-  var obj = JSON.parse(String(fs.readFileSync('./snipe.json')))
-
-        obj[message.guild.id] = JSON.parse(JSON.stringify(message))
-        fs.writeFileSync('./snipe.json', JSON.stringify(obj))
+    var obj = JSON.parse(String(fs.readFileSync('./snipe.json')))
+    
+    obj[message.guild.id] = JSON.parse(JSON.stringify(message))
+    fs.writeFileSync('./snipe.json', JSON.stringify(obj))
 })
 client.login(process.env.TOKEN);
