27094ca9659a7a9642feaf4c693ab4cbd54349cb
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-12 09:17:41 -0500

    Update snipe.js

--- handlers/commands/Info/snipe.js
+++ handlers/commands/Info/snipe.js
@@ -8,38 +8,32 @@ module.exports = {
     usage: "snipe",
     category: "Moderation",
     execute: async (message, args, client) => {
-    // hello dark "...... GASP"
-
-
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-        return message.reply("Missing Permissions!");
-    }
-
-    var obj = JSON.parse(String(fs.readFileSync('./snipe.json')))
-
-    if (obj[message.guild.id]) {
-      const msg = obj[message.guild.id]
-
-      const embed = new Discord.MessageEmbed()
-        .setTitle('Sniped message')
-        .addField('Author', `<@${msg.authorID}>`)
-        .addField('Channel', `<#${msg.channelID}>`)
-        .addField('Embeds', msg.embeds.length, true)
-        .addField('Attachments', msg.attachments.length, true)
-        .addField('Content', `
+        
+        if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+            return message.reply("Missing Permissions!");
+        }
+        
+        var obj = JSON.parse(String(fs.readFileSync('./snipe.json')))
+        
+        if (obj[message.guild.id]) {
+            const msg = obj[message.guild.id]
+            
+            const embed = new Discord.MessageEmbed()
+                .setTitle('Sniped message')
+                .addField('Author', `<@${msg.authorID}>`)
+                .addField('Channel', `<#${msg.channelID}>`)
+                .addField('Embeds', msg.embeds.length, true)
+                .addField('Attachments', msg.attachments.length, true)
+                .addField('Content', `
         > ${msg.content}
         `)
-        .setTimestamp()
-        .setFooter(`Requested by ${message.author.tag}`)
-        .setColor('RANDOM')
-
-      message.channel.send(embed)
-     // message.channel.send(`From <@${msg.authorID}>
-  //  Channel: <#${msg.channelID}>
-   //   > ${msg.content}
-   //   `)
-    } else {
-      message.channel.send('No message found in this guild!')
+                .setTimestamp()
+                .setFooter(`Requested by ${message.author.tag}`)
+                .setColor('RANDOM')
+            
+            message.channel.send(embed)
+        } else {
+            message.channel.send('No message found in this guild!')
+        }
     }
-}
 }
